43b91fbd091fb5df246917e25f0ab9c6e7716871
Nishanth Gaddam <nishanth.gaddam37@gmail.com>
2021-04-13 20:46:05 -0500

    Added heapSortAnimations()...Modified bar length and count of bars

--- src/component/SortingVisualizer/SortingVisualizer.jsx
+++ src/component/SortingVisualizer/SortingVisualizer.jsx
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
 import './SortingVisualizer.css';
 import * as sortingAlgorithum from '../SortingAlgo/sortmerge.js';
-
+import * as sortingHeap from '../SortingAlgo/heapSort.js';
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
 
 // Change this value for the number of bars (value) in the array.
-const NUMBER_OF_ARRAY_BARS = 100;
+const NUMBER_OF_ARRAY_BARS = 110;
 
 // This is the main color of the array bars.
 const PRIMARY_COLOR = 'turquoise';
@@ -30,7 +30,7 @@ class SortingVisualizer extends Component{
     resetArray(){
         const array=[];
         for(let i=0;i<NUMBER_OF_ARRAY_BARS; i++){
-            array.push(randomIntFromIntervals(5,200));
+            array.push(randomIntFromIntervals(50,650));
         }
         this.setState({array});
     }
@@ -41,7 +41,7 @@ class SortingVisualizer extends Component{
 
         const animations=sortingAlgorithum.mergeSort(this.state.array);
 
-       // console.log(sortedArray);
+        console.log('Merge Sort');
         //console.log(arraysAreEqual(javaScriptSortedArray,sortedArray));
 
 
@@ -74,8 +74,40 @@ class SortingVisualizer extends Component{
     quickSort(){
 
     }
-    heapSort(){
+    heapSortAnimations(){
+        const javaScriptSortedArray=this.state.array.slice()
+        .sort((a,b)=>a-b);
 
+        const animations=sortingHeap.heapSortAnimations(this.state.array);
+
+       // console.log(sortedArray);
+        //console.log(arraysAreEqual(javaScriptSortedArray,sortedArray));
+
+
+
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 4 <= 1;
+            if (isColorChange) {
+              const [barOneIdx, barTwoIdx] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              const barTwoStyle = arrayBars[barTwoIdx].style;
+              const color = i % 4 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+              setTimeout(() => {
+                barOneStyle.backgroundColor = color;
+                barTwoStyle.backgroundColor = color;
+              }, i * ANIMATION_SPEED_MS);
+            } else {
+              setTimeout(() => {
+                const [barOneIdx, newHeight] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                barOneStyle.height = `${newHeight}px`;
+                this.state.array[barOneIdx]={newHeight};
+              }, i * ANIMATION_SPEED_MS);
+            }
+          }
+          console.log('heapSort');
+          console.log(arraysAreEqual(javaScriptSortedArray,this.state.array));
     }
 
     render(){
@@ -87,7 +119,7 @@ class SortingVisualizer extends Component{
 <button onClick={()=> this.resetArray()}> Generate New Array </button>
                 <button onClick={()=> this.mergeSrt()}> Merge Sort</button>
                 <button onClick={()=> this.quickSort()}> Quick Sort</button>
-                <button onClick={()=> this.heapSort()}> Heap Sort </button>
+                <button onClick={()=> this.heapSortAnimations()}> Heap Sort </button>
                 <button onClick={()=> this.insertSort()}> Insertion Sort </button>
                 <button onClick={()=> this.heapSort()}> Bubble Sort </button>
                 <button onClick={()=> this.resetArray()}> Reset </button>
