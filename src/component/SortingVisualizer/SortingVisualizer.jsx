dc478f5845528bed4606fcb0dbf841c88b719b8b
satish1015 <satish.pdonur@gmail.com>
2021-04-14 08:44:17 -0500

    SortingVisualizer.jsx file updated for Quick Sort Algorithm (added quickSort()) and updated quickSort.js file

--- src/component/SortingVisualizer/SortingVisualizer.jsx
+++ src/component/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import './SortingVisualizer.css';
 import * as sortingAlgorithum from '../SortingAlgo/sortmerge.js';
+import {getQuickSortAnimations} from '../SortingAlgo/quickSort.js';
 
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
